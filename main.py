f4374b27765b7c06410697170e78c4ef64f8feb5
Naomi Johnson <snjohnson789@gmail.com>
2024-05-28 21:38:21 -0700

    Updating to ST7735R???

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import board
 import displayio
-from adafruit_st7789 import ST7789
+# from adafruit_st7789 import ST7789
+from adafruit_st7735r import ST7735R
 import busio
 import digitalio
 import time
 
-print("Imports done")
 
 # forked https://github.com/dfinein/Pico-LCd-114/blob/main/main.py
 displayio.release_displays() # what does this do???
@@ -14,46 +14,43 @@ mosi = board.GP11
 spi = busio.SPI(chip_spi, mosi)
 tft_cs = board.GP9 # chip selection
 tft_dc = board.GP8 # data command
-key_0 = digitalio.DigitalInOut(board.GP15)
-key_1 = digitalio.DigitalInOut(board.GP17)
-key_2 = digitalio.DigitalInOut(board.GP2)
-key_3 = digitalio.DigitalInOut(board.GP3)
+key_0 = board.GP15
+key_1 = board.GP17
+key_2 = board.GP2
+key_3 = board.GP3
 reset = board.GP12
 backlight = board.GP13
 
-print("Accessed board")
+# Define keys
+key_0 = digitalio.DigitalInOut(board.GP15)
+key_0.switch_to_input(pull=digitalio.Pull.DOWN)
 
+key_1 = digitalio.DigitalInOut(board.GP17)
+key_1.switch_to_input(pull=digitalio.Pull.DOWN)
+
+key_2 = digitalio.DigitalInOut(board.GP2)
+key_2.switch_to_input(pull=digitalio.Pull.DOWN)
+
+key_3 = digitalio.DigitalInOut(board.GP3)
+key_3.switch_to_input(pull=digitalio.Pull.DOWN)
 
 # forked from: https://docs.circuitpython.org/projects/st7789/en/latest/examples.html#x135
+# adjusted from ST7789 to ST7735R (which includes the S version we're using):
+# https://github.com/adafruit/Adafruit_CircuitPython_ST7735R
 display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=reset)
-display = ST7789(
+display = ST7735R(
     display_bus,
     rotation=180,
     width=128,
     height=128,
-    rowstart=0, # idk why 80??? why not (0,0)???
+    colstart=2,
+    rowstart=1, # idk why 80??? why not (0,0)???
     backlight_pin=backlight,
 )
 
-print("Set display")
-
-# Define keys
-key_0.switch_to_input(pull=digitalio.Pull.UP)
-key_1.switch_to_input(pull=digitalio.Pull.UP)
-key_2.switch_to_input(pull=digitalio.Pull.UP)
-key_3.switch_to_input(pull=digitalio.Pull.UP)
-    
-print("Defined keys")
-
-bitmap = displayio.OnDiskBitmap("/images/white.bmp")
-# forked from: https://learn.adafruit.com/circuitpython-display-support-using-displayio/display-a-bitmap
-tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
-group = displayio.Group()
-group.append(tile_grid)
-display.root_group = group
 
 while True:
-    if not key_0.value:
+    if key_0.value:
         print("Key 0 pressed")
         # convert png to index bmp: https://learn.adafruit.com/creating-your-first-tilemap-game-with-circuitpython/indexed-bmp-graphics
         bitmap = displayio.OnDiskBitmap("/images/hello-world.bmp")
@@ -62,39 +59,15 @@ while True:
         group = displayio.Group()
         group.append(tile_grid)
         display.root_group = group
-        time.sleep(3)
-    if not key_1.value:
+    if key_1.value:
         print("Key 1 pressed")
-        bitmap = displayio.OnDiskBitmap("/images/hug-alligator-pig.bmp")
-        # forked from: https://learn.adafruit.com/circuitpython-display-support-using-displayio/display-a-bitmap
-        tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
-        group = displayio.Group()
-        group.append(tile_grid)
-        display.root_group = group
-        time.sleep(3)
-    if not key_2.value:
+        # Do something for key 1 press
+    if key_2.value:
         print("Key 2 pressed")
-        bitmap = displayio.OnDiskBitmap("/images/01-flower.bmp")
-        # forked from: https://learn.adafruit.com/circuitpython-display-support-using-displayio/display-a-bitmap
-        tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
-        group = displayio.Group()
-        group.append(tile_grid)
-        display.root_group = group
-        time.sleep(3)
-    if not key_3.value:
+        # Do something for key 2 press
+    if key_3.value:
         print("Key 3 pressed")
-        bitmap = displayio.OnDiskBitmap("/images/11-flowers.bmp")
-        # forked from: https://learn.adafruit.com/circuitpython-display-support-using-displayio/display-a-bitmap
-        tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
-        group = displayio.Group()
-        group.append(tile_grid)
-        display.root_group = group
-        time.sleep(3)
-    else:
-        bitmap = displayio.OnDiskBitmap("/images/white.bmp")
-        # forked from: https://learn.adafruit.com/circuitpython-display-support-using-displayio/display-a-bitmap
-        tile_grid = displayio.TileGrid(bitmap, pixel_shader=bitmap.pixel_shader)
-        group = displayio.Group()
-        group.append(tile_grid)
-        display.root_group = group
-        time.sleep(3)    
+        # Do something for key 3 press
+    # Add a small delay to avoid CPU intensive polling
+    time.sleep(2)
+
